bf41f0b68902a29fec849285d3cf70aefd46092e
fabiorengel <fabiorengel@gmail.com>
2022-01-28 20:50:34 -0300

    Vverficação linha disponiv OK!

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 let bolas = document.getElementsByClassName('bola')
-console.log(bolas)
 
 
 let mT =    //matrix Tabuleiro = representa 7 colunas // 6 linhas de jogadas
-        [
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-        ]
-        console.log(mT)
-        
-        
-        function col(x, y, player) {
-            
-            let nMatrix = Number(x)+Number(y)*0
-            bolas[nMatrix].style.backgroundColor = 'yellow'
-            mT[x][y] = player
-            console.log(mT)
-            
+[
+    [0,0,0,0,0,0], // coluna 0 
+    [0,0,0,0,0,0], // coluna 1 
+    [0,0,0,0,0,0], // coluna 2 
+    [0,0,0,0,0,0], // coluna 3 
+    [0,0,0,0,0,0], // coluna 4 
+    [0,0,0,0,0,0], // coluna 5 
+    [0,0,0,0,0,0]  // coluna 6 
+]
+
+//console.log(mT)
+
+function casaLivre(x,y) {
+    console.log(x + y)
+
+    while (mT[x][y] != 0 && y > 0) {
+        y--
+    } 
+    if (mT[x][y] != 0) {
+        return [false]
+    }
+    else {
+        return [true, y]
+    }
+    
 }
+
+function col(x, y, player) {
+    console.log(x)
+let cLivre = casaLivre(x,y) 
+//console.log(cLivre[0],cLivre[1])    
+    if (cLivre[0]) {
+        let nMatrix = Number(x)+Number(cLivre[1])*7
+        bolas[nMatrix].style.backgroundColor = 'yellow'
+        mT[x][cLivre[1]] = player
+    }
+}    
+//console.log(mT)
+
+
+//return true
 
 /*function linhaDisponivel(Col,lin, player) {
     while (mT[col][lin] != 0 || linha >= 0 ){ 
@@ -36,6 +57,7 @@ let mT =    //matrix Tabuleiro = representa 7 colunas // 6 linhas de jogadas
     } 
 }
 
+console.log(bolas)
 let n = 2
 
 
