bf4c16e56d4a8f795d2758955cfb4649497e62e3
fabiorengel <fabiorengel@gmail.com>
2022-01-29 23:03:02 -0300

    V

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 let bolas = document.getElementsByClassName('bola')
 let statusJogo = document.getElementById('status')
-let mT     //matrix Tabuleiro = representa 7 colunas // 6 linhas de jogadas - inicializada em reiniciarPartida
-let currentStatus   // Armazena o estado atual do jogo:
-// 0 = Vez no Player 1
-// 1 = Vez do NPC / Player 2
-// 2 = Partida Terminada = Player 1 Ganhou
-// 3 = Partida Terminada = NPC / Player 2 Ganhou */
-// inicializada em reiniciarPartida
+let mT     
+/*matrix Tabuleiro = representa 7 colunas // 6 linhas de jogadas - inicializada em reiniciarPartida*/
+let currentStatus   
+
+/*  currentStatus: Armazena o estado atual do jogo:
+    0 = Vez no Player 1
+    1 = Vez do NPC / Player 2
+    2 = Partida Terminada = Player 1 Ganhou
+    3 = Partida Terminada = NPC / Player 2 Ganhou 
+    inicializada em reiniciarPartida
+*/
 
     function alteraStatus(x) {
         currentStatus = x
         if (x==1) {
-            statusJogo.innerText = 'Vez do computador jogar'
-            vaiComputador() // chama o NPC
+            statusJogo.innerText = 'Vez do computador jogar...'
+            setTimeout(vaiComputador, 100) // chama o NPC
         }
         else if (x==2) {
-            statusJogo.innerText = 'Oxê, você ganhou!'
+            statusJogo.innerText = 'Quem diria? Você ganhou!'
         }
         else if (x==3) {
             statusJogo.innerText = 'Achou que podia me ganhar, né? Achou errado!'
@@ -43,10 +47,16 @@ let currentStatus   // Armazena o estado atual do jogo:
         if (currentStatus == 0) {
             let cLivre = casaLivre(x,y) 
             if (cLivre[0]) {
-                let posVetor = Number(x)+Number(cLivre[1])*7 //
-                bolas[posVetor].style.backgroundColor = 'yellow'
-                mT[x][cLivre[1]] = player
-                alteraStatus(1)
+                // Daqui
+                    let posVetor = Number(x)+Number(cLivre[1])*7 //
+                    bolas[posVetor].style.backgroundColor = 'yellow'
+                // até aqui, o código altera a cor da bola equivalente, trasformando a matriz 6x7 em um vetor de 42 posições. (div.bola 0a41)
+
+                mT[x][cLivre[1]] = player //Salva na matriz a posição que a nova bola ocupa.
+                zzzConfereGanhador(x, cLivre[1], player)
+                if (currentStatus==0){                
+                    alteraStatus(1) // Função que muda status do jogo (desde a vez de qual jogador até o termino da partida)
+                }
             }
             else {
                 alert('Jogada Inválida')
@@ -60,30 +70,7 @@ let currentStatus   // Armazena o estado atual do jogo:
         }
     }    
 
-    function vaiComputador() {
-        // Jogada do computador.
-        let aux = true
-        let aux2
-        let x = 0
-        while  (x < 7 && aux)  {
-            aux2 = casaLivre(x,5) 
-            if (aux2[0]) {
-                aux=false
-            }
-            else {
-            x++   
-            }      
-        }
-        if (!aux) { // if termporario para parar o programa quando a matriz estiver cheia. No futuro não será necessário
-            let posVetor = Number(x)+Number(aux2[1])*7 
-            bolas[posVetor].style.backgroundColor = 'red'
-            mT[x][aux2[1]] = 'npc'
-            alteraStatus(0)
-        }
-    }
-
-    function reiniciarPartida() 
-    {
+    function reiniciarPartida() {
         
         mT =    //matrix Tabuleiro = representa 7 colunas // 6 linhas de jogadas
         // Dá pra fazer por laço, mas não é interessante.
@@ -96,6 +83,70 @@ let currentStatus   // Armazena o estado atual do jogo:
             [0,0,0,0,0,0], // coluna 5 
             [0,0,0,0,0,0]  // coluna 6 
         ]
-
         currentStatus = 0  
     }
+
+    function vaiComputador() {
+        // Jogada do computador.
+        let aux = true
+        let aux2
+        let x = 0
+
+        while  (x < 7 && aux) { 
+        /*Procura e todo tabuleiro, a partir da coluna 0 (primeira à esquerda), uma casa livre para jogar. */
+            aux2 = casaLivre(x,5) 
+            if (aux2[0]) {
+                aux=false
+            }
+            else {
+                x++   
+            }      
+        }
+
+        if (!aux) 
+        // if termporario para parar o programa quando a matriz estiver cheia. No futuro não será necessário
+            { 
+                //Daqui
+                let posVetor = Number(x)+Number(aux2[1])*7 
+                bolas[posVetor].style.backgroundColor = 'red'
+                // até aqui, o código altera a cor da bola equivalente, trasformando a matriz 6x7 em um vetor de 42 posições. (div.bola 0a41)
+                mT[x][aux2[1]] = 'npc'
+                
+                zzzConfereGanhador(x,aux2[1],'npc')
+         
+                if (currentStatus == 1){ 
+                    alteraStatus(0)
+                }
+            }
+        }
+        
+        function confVerticalPraBaixo(x,y) { 
+            let contSeguidas = 1
+            auxBreak = true
+            while (y < 6 && auxBreak==true) {
+                if (mT[x][y] == mT[x][y+1]) { 
+                    contSeguidas++
+                    y++
+                }
+                else{
+                    auxBreak=false
+                }
+            }
+            return contSeguidas
+        }
+        function zzzConfereGanhador (posX, posY, player) {
+            let Ganhou = confVerticalPraBaixo(posX,posY)
+
+            if (Ganhou >= 4 ) {
+                if  (player =='npc') {
+                       alteraStatus(3)
+                }
+                else {
+                    alteraStatus(2)
+                }
+                setTimeout(alert, 1000, 'Outra Partida?')
+            }
+        }
+        
+        /*console.log(mT)
+        console.log(x,aux2[1],'npc')*/
